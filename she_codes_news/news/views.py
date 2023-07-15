316ba980335a8a5ae7d87177c7acdfa6295967ff
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-07-15 11:48:20 +1000

    created new user app

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -13,7 +13,8 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['latest_stories'] = NewsStory.objects.order_by('-pub_date')[:4]
+        # print(NewsStory.object.all()[:4])
         return context
     
 class StoryView(generic.DetailView):
@@ -27,4 +28,14 @@ class AddStoryView(generic.CreateView):
     template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
 
+# Update story creation view - To replace that text field, we need to tell our view to insert the current user's info whenthey submit a new story. 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
     # good place to add to project - take to other place, view of what you've written?
+
+
+
+
+# use the print function to see what you are doing and help you debug
--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'users.apps.UsersConfig',
+    ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +128,7 @@ STATICFILES_DIRS = (
     )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'news:index'
+LOGOUT_REDIRECT_URL = 'news:index'
+
