3b2999890ae0f1747257d6b319beb47f932eb051
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-07-17 21:28:35 +1000

    account button to nav, images to articles.

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -6,6 +6,7 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+# CustomUserEDITFOrm?????
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
