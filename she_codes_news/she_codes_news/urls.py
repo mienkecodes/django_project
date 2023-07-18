5cd811b10a29d4cc77f9230ed7a126906d26c809
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-07-18 11:26:09 +1000

    Started with css

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path("create-account/", include("users.urls"))
+    
 ]
